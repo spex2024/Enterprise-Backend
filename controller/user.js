d19c448b79a54eee41b30947d6f019a53089b63a
spex2024 <dercolbags@gmail.com>
2024-08-18 23:30:39 +0000

    v1.0.0.0

--- controller/user.js
+++ controller/user.js
@@ -260,7 +260,6 @@ export const signIn = async (req, res) => {
         res.cookie('token', token, {
             httpOnly: true,
             secure: process.env.NODE_ENV === 'production',
-            sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax',
             maxAge: 24 * 60 * 60 * 1000, // 1 hour
         });
 
