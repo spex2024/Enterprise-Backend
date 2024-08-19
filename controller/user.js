8d4c243ff597ad562a1bbb9df0229f64850f95cc
spex2024 <dercolbags@gmail.com>
2024-08-19 09:59:24 +0000

    v1.0.0.0

--- controller/user.js
+++ controller/user.js
@@ -436,7 +436,7 @@ export const updateUserInfo = async (req, res) => {
 
 
         try {
-            const user = await User.findById(req.user.id);
+            const user = await User.findById(req.user._id);
 
             if (!user) {
                 return res.status(404).json({ message: "User not found" });
